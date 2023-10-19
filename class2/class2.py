30b4db54c6de8d1945231c7156588bcfac625c1c
Danish25 <dansire7@gmail.com>
2023-10-18 23:19:52 -0700

    Class2

---
+++ class2/class2.py
@@ -0,0 +1,15 @@
+name : str = "Danish Ghani"
+father_name: str = "Abdul Hameed"
+education: str = "Masters"
+Age : int = 38
+
+# usage of triple single / double quotes
+
+card : str = f"""
+PIAIC
+Student Name: {name}
+Father's Name: {father_name}
+Age: {Age}
+"""
+
+print(card)
